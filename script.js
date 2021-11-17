e9755d2947a8ec28cff8c1abc470f4053ebe38b0
Victoria_T <Tabordaingridvictoria@outlook.es>
2021-11-17 11:08:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let formulario = document.querySelector('#link')
+formulario.addEventListener('mouseover', ()=>{
+    formulario.style.width = '600px'
+    formulario.style.color = 'white'
+    formulario.style.transition = 'all 1s'
+})
+formulario.addEventListener('mouseout', ()=>{
+    formulario.style.width = '100%'
+    formulario.style.color = 'pink'
+    formulario.style.transition = 'all 1s'
+})
+
+
+
+let formulario2 = document.querySelector('#link1')
+formulario2.addEventListener('mouseover', ()=>{
+    formulario2.style.width = '600px'
+    formulario2.style.color = 'white'
+    formulario2.style.transition = 'all 1s'
+})
+formulario2.addEventListener('mouseout', ()=>{
+    formulario2.style.width = '100%'
+    formulario2.style.color = 'pink'
+    formulario2.style.transition = 'all 1s'
+})
+
+
+
+let formulario3 = document.querySelector('#link2')
+formulario3.addEventListener('mouseover', ()=>{
+    formulario3.style.width = '600px'
+    formulario3.style.color = 'white'
+    formulario3.style.transition = 'all 1s'
+})
+formulario3.addEventListener('mouseout', ()=>{
+    formulario3.style.width = '100%'
+    formulario3.style.color = 'pink'
+    formulario3.style.transition = 'all 1s'
+})
